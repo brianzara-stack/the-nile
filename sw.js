f862a468740eb9c8e47d60f766de576b239fe3e9
brianzara-stack <brianzara@gmail.com>
2026-06-05 17:31:52 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='nile-v2';const SHELL=['/','/index.html','/css/main.css','/js/app.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(!e.request.url.includes('supabase')&&!e.request.url.includes('listennotes')&&e.request.method==='GET'){e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('/index.html'))))}});
